4e3f9db836f14593ebda04b3d4dbf36d9d99895e
Tribe Leader <67007753+badb000m@users.noreply.github.com>
2021-03-22 00:59:58 +0000

    Update index.py
    main

--- index.py
+++ index.py
@@ -1,1 +1,31 @@
+from discord import TextChannel
+from discord.ext.commands import Bot, Context
+from json import load
 
+with open("config.json") as f:
+    config = load(f)
+
+bot = Bot(command_prefix=config["prefix"])
+
+@bot.event
+async def on_ready():
+    async for guild in bot.fetch_guilds(limit=None):
+        #Fetches all guilds
+        print(guild.name)
+
+@bot.command(aliases=["spam", "sm", "cs", "rape", "crape", "cr"])
+async def _webhook_channel_spammer(ctx:Context, arg:str=None):
+    """Proof of concept of webhook/channel spammer in just one line!
+    Equivelant to: ```py
+    for channel in ctx.guild.channels:
+        if type(channel) == TextChannel:
+            for channel in await channel.webhooks():
+                channel.send(arg or config.get("raid_message"))
+    ```
+    More of a WIP thing since it is kind of slow, but you get the point!
+    """
+    [
+        await channel.send(arg or config.get("raid_message")) for channel in ctx.guild.channels if type(channel) == TextChannel for channel in await channel.webhooks()
+    ]
+
+bot.run(config.get("token"))
